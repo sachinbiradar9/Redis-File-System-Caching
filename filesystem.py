dfca989870dac753b0a7a8b5910a1c9c6735d903
Ubuntu <ubuntu@ip-172-31-88-119.ec2.internal>
2018-02-25 12:45:08 +0000

    cleaned fuse

--- filesystem.py
+++ filesystem.py
@@ -179,5 +179,5 @@ if __name__ == '__main__':
     parser.add_argument('mount')
     args = parser.parse_args()
 
-    #logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.DEBUG)
     fuse = FUSE(Memory(), args.mount, foreground=True, allow_other=False)
